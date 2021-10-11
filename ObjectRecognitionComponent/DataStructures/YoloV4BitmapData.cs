86a80644db0dc8cf4f28835878e79f998ada2102
Zubkov Egor <69621666+AldOgen@users.noreply.github.com>
2021-10-11 05:48:41 +0300

    Add files via upload

---
+++ ObjectRecognitionComponent/DataStructures/YoloV4BitmapData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Image;
+using System.Drawing;
+
+namespace ObjectRecognitionComponent.DataStructures {
+    public class YoloV4BitmapData {
+        [ColumnName("bitmap")]
+        [ImageType(416, 416)]
+        public Bitmap Image { get; set; }
+
+        [ColumnName("width")]
+        public float ImageWidth => Image.Width;
+
+        [ColumnName("height")]
+        public float ImageHeight => Image.Height;
+    }
+}
